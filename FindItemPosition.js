b671b610d92277b4955abf48a3ab1b043aeeaa44
sonpal521 <119480099+sonpal521@users.noreply.github.com>
2022-12-05 09:59:49 +0530

    Add files via upload

---
+++ FindItemPosition.js
@@ -0,0 +1,24 @@
+
+//Item delivery needs to take place for a circle
+// of size B. Suppose we start from the position C (given) 
+//then find the position where the Ath item will be delivered.
+
+
+// B ==> Size of circle
+// A ==> Number of items
+// C ==> Initial position
+ 
+function lastPosition(A, B, C)
+{
+    if (A <= B - C + 1)
+    return A + C - 1;
+
+    A = A - (B - C + 1);
+    return (A % B == 0) ? B : (A % B);
+}
+ 
+// Driver code
+let A = 8;
+let B = 5;
+let C = 2;
+console.log(lastPosition(A, B, C));
